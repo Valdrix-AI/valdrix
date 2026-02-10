88212d5612d2e59f8412c44b0faf8118a86a724d
daretechie <deeprince2020@gmail.com>
2026-02-10 19:09:15 +0100

    chore: remove stale debug/test artifacts and refresh cleanup categorization

--- debug_llm_usage.py
+++
@@ -1,7 +0,0 @@
-from app.models.llm import LLMUsage
-print(LLMUsage.__table__.columns.keys())
-try:
-    u = LLMUsage(operation_id="test")
-    print("Success")
-except Exception as e:
-    print(e)
